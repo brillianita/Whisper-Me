9493dc9d9851627f41f4e8270c3b72e9e9b81544
@brillianita <brillianitaaa@gmail.com>
2024-07-27 15:03:34 +0800

    feat: adding premium plan, footer section

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -6,11 +6,32 @@ import {
   MessageSquareText,
   KeyRound,
   Pointer,
-  Users
+  Users,
+  Facebook,
+  Twitter,
+  Instagram,
 } from 'lucide-react'
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "./components/ui/button"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: false,
+      mirror: true
+    });
+  }, []);
   return (
     <>
       {/* Navigation */}
@@ -43,28 +64,25 @@ export default function Home() {
       {/* Jumbotron */}
       <div className="relative w-full h-[700px] overflow-hidden-lg">
         <img src="../src/assets/bg-jumbotron-4.png" alt="Jumbotron Background" className="absolute inset-0 w-full h-full object-cover" />
-        <div className="absolute inset-0 flex flex-col items-center justify-center p-8 text-center">
-          <div className="space-y-4">
+        <div className="absolute inset-0 flex flex-col justify-center p-8 text-center">
+          <div className="space-y-3">
             <div className="flex justify-center items-center">
               <h1 className="text-4xl font-bold text-white sm:text-5xl md:text-6xl mr-3">WhisperMe</h1>
-              <h1 className="text-8xl font-bold text-white mr-3 text-navy animate-pulse">!</h1>
+              <h1 className="text-8xl font-bold mr-3 text-navy animate-pulse">!</h1>
             </div>
             <p className="text-lg text-white md:text-xl">
               "Whisper Your Secrets, Hear the Unknown – Dive into the World of Anonymous Chats!"
             </p>
-            {/* <a
-              href="#"
-              className="inline-flex items-center justify-center rounded-md bg-primary px-6 py-3 text-sm font-medium text-primary-foreground transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-            >
-              Get Started
-            </a> */}
           </div>
         </div>
       </div>
 
       {/* Why WhisperMe? */}
-      <div>
-        <h2 className="text-4xl font-bold text-center">Why Use WhisperMe ?</h2>
+      <div data-aos="zoom-in-up">
+        <div className="mx-auto">
+          <h2 className="text-4xl font-bold text-center pb-5">Why Use WhisperMe ?</h2>
+          <div className="w-[200px] border-t-[4px] border-navy mx-auto"></div>
+        </div>
         <div className="grid grid-cols-2 gap-2 mt-10">
           <div className="flex flex-col justify-center items-center text-center">
             <div className="flex bg-dark-orange rounded-full items-center size-16 justify-center mb-3 place-self-center">
@@ -101,17 +119,88 @@ export default function Home() {
       </div>
 
       {/* Get Your Premium */}
-      <div className="mt-10 bg-peach p-2">
-        <h2 className="bg-dark-orange rounded-3xl text-3xl font-bold text-center text-white mt-5 h-16 w-auto">Choose Your WhisperMe! Experience</h2>
-        <div>
-          <h5 className="font-bold text-2xl">Free Plan</h5>
-          <p className=""><b>Limited Insight: </b>Access up to 100 responses. Perfect for casual users who want a taste of anonymous feedback.</p>
-        </div>
-        <div>
-          <h5>Premium Plan</h5>
-          <p> <b>Unlimited Responses: </b>Dive deep with unlimited responses. Ideal for those who seek extensive interactions and full insights without boundaries.</p>
+
+      {/* <div className="bg-blue/[0.3] p-5 mx-auto mt-10"> */}
+      <div className="relative w-full h-[600px] overflow-hidden-lg mt-10 p-4">
+        <img src="../src/assets/bg-premium-plan-2.png" alt="Jumbotron Background" className="absolute inset-0 w-full h-full object-cover" />
+        <div className="absolute mx-auto w-full pt-5">
+          <div className="mx-auto">
+            <h2 data-aos="zoom-in" className="text-4xl font-bold text-center pb-5">Choose Your WhisperMe! Experience</h2>
+            <div className="w-[200px] border-t-[4px] border-dark-orange mx-auto"></div>
+          </div>
+          {/* <h2 className="text-4xl font-bold text-center text-black mt-5 h-16 w-auto">Choose Your WhisperMe! Experience</h2> */}
+          <div className="flex justify-center items-center mt-10">
+            <Card data-aos="flip-left" className="w-[350px] h-[300px] shadow-2xl">
+              <CardHeader>
+                <CardTitle>Free Plan</CardTitle>
+                <CardContent className="text-center text-5xl font-bold text-navy"><h1>$0</h1></CardContent>
+                <CardDescription className="text-black"><p className=""><b>Limited Insight: </b>Access up to 100 responses. Perfect for casual users who want a taste of anonymous feedback.</p></CardDescription>
+              </CardHeader>
+              <CardFooter className="flex justify-center">
+                <Button className="bg-navy hover:bg-dark-orange hover:text-navy">Try Now!</Button>
+              </CardFooter>
+            </Card>
+            <Card data-aos="flip-right" className="w-[350px] h-[340px] shadow-2xl bg-peach">
+              <CardHeader>
+                <CardTitle>Premium Plan</CardTitle>
+                <CardContent className="text-center text-5xl font-bold text-navy"><h1>$10</h1></CardContent>
+                <CardDescription className="text-black"> <p><b>Unlimited Responses: </b>Dive deep with unlimited responses. Ideal for those who seek extensive interactions and full insights without boundaries.</p></CardDescription>
+              </CardHeader>
+              <CardFooter className="flex justify-center">
+                <Button className="bg-navy hover:bg-dark-orange hover:text-navy">Try Now!</Button>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
       </div>
+
+      {/* Footer */}
+      <footer className="p-4 h-[250px] mt-[20px]">
+        <div className="container mx-auto flex flex-wrap justify-between">
+          <div className="flex items-center">
+            <h6 className="font-bold text-3xl">WhisperMe</h6>
+            <h6 className="font-bold text-6xl text-navy">!</h6>
+          </div>
+          <div>
+            <h6 className="font-bold">Why Us</h6>
+            <div className="border-t-[3px] w-10 border-navy"></div>
+            <div className="pt-3">
+              <p className="pb-3">Receive Honest Feedback</p>
+              <p className="pb-3">Maintain Privacy</p>
+              <p className="pb-3">Simple to Use</p>
+              <p>No Sign-Up Needed for Responders</p>
+            </div>
+          </div>
+          <div>
+            <h6 className="font-bold">Follow Us</h6>
+            <div className="border-t-[3px] w-10 border-navy"></div>
+            <div className="pt-4">
+              <div className="flex bg-dark-orange rounded-full items-center size-[45px] justify-center mb-3">
+                <Facebook color="white" size={30} className="" />
+              </div>
+              <div className="flex bg-dark-orange rounded-full items-center size-[45px] justify-center mb-3">
+                <Instagram color="white" size={30} className="" />
+              </div>
+              <div className="flex bg-dark-orange rounded-full items-center size-[45px] justify-center mb-3">
+                <Twitter color="white" size={30} className="" />
+              </div>
+
+            </div>
+          </div>
+          <div>
+            <h6 className="font-bold">Quick Links</h6>
+            <div className="border-t-[3px] w-10 border-navy"></div>
+            <div className="pt-3">
+              <p className="pb-3"><a href="/">Home</a></p>
+              <p className="pb-3"><a href="/">SignIn</a></p>
+              <p className="pb-3"><a href="/">SignUp</a></p>
+            </div>
+          </div>
+        </div>
+        <div className="text-center mt-[50px] border-t-2 border-dark-orange pt-2 pb-3">
+          © 2024 WhisperMe. All rights reserved.
+        </div>
+      </footer>
     </>
   )
 }
